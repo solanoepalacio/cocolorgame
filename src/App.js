1a2c91aed112a30f34fff8396bb7820d06de5dcb
solanoe <solano.palacio@rappi.com>
2021-02-05 01:34:22 -0300

    add atics call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import _ from 'lodash';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import GameSetup from './game/GameSetup';
 import GamePlay from './game/GamePlay';
 import ScoreBoard from './game/ScoreBoard';
 
+const track = () => {
+  const referrer = document.referrer;
+  const browser = _.get(window, 'navigator.appName');
+  const platform = _.get(window, 'navigator.platform');
+  fetch('https://sf27prmmu5.execute-api.us-east-1.amazonaws.com/dev/session/start', {
+    method: 'POST',
+    body: JSON.stringify({
+      referrer, platform, browser,
+    }),
+  })
+};
+
 function App() {
+  useEffect(() => {
+    track();
+  });
   const [ gameSetup, setGameSetup ] = useState(null);
   const [ gameScoreBoard, setGameScoreBoard ] = useState(null);
 
